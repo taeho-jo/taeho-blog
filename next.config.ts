1aeb5fa2a2a30459083ad4bc6f34cd7bdc2a3440
taeho-jo <jotang3726@gmail.com>
2025-08-03 15:47:59 +0900

    feat : local 테스트 중

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import rehypePrism from 'rehype-prism-plus'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
   // Optionally, add any other Next.js config below
