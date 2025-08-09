65b30bd173798cf170f14ae076a697e11dc43fde
Jotang <jotang3726@gmail.com>
2025-08-10 07:07:00 +0900

    feat: [DarkModeToggle 컴포넌트 리팩토링 및 DropdownMenu 추가]
    - DarkModeToggle 테마 전환 기능 DropdownMenu 기반으로 리팩토링
    - @radix-ui/react-dropdown-menu 라이브러리 추가 및 적용

--- src/components/layout/DarkModeToggle.tsx
+++ src/components/layout/DarkModeToggle.tsx
@@ -3,6 +3,12 @@
 import { useTheme } from 'next-themes'
 import { Moon, Sun, Laptop } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
 export default function DarkModeToggle() {
   const { theme, setTheme } = useTheme()
@@ -12,40 +18,33 @@ export default function DarkModeToggle() {
   }
 
   return (
-    <div className="flex space-x-2">
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => handleThemeChange('light')}
-        className={
-          theme === 'light'
-            ? 'bg-gray-700 text-highlight hover:text-highlight'
-            : 'transition-colors duration-200 hover:text-highlight'
-        }>
-        <Sun className="h-5 w-5" />
-      </Button>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => handleThemeChange('dark')}
-        className={
-          theme === 'dark'
-            ? 'bg-gray-700 text-highlight hover:text-highlight'
-            : 'transition-colors duration-200 hover:text-highlight'
-        }>
-        <Moon className="h-5 w-5" />
-      </Button>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => handleThemeChange('system')}
-        className={
-          theme === 'system'
-            ? 'bg-gray-700 text-highlight hover:text-highlight'
-            : 'transition-colors duration-200 hover:text-highlight'
-        }>
-        <Laptop className="h-5 w-5" />
-      </Button>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          className={
+            'text-highlight transition-colors duration-200 hover:text-highlight'
+          }
+          variant="outline"
+          size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => handleThemeChange('light')}>
+          <Sun className="mr-2 h-4 w-4" />
+          <span>Light</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeChange('dark')}>
+          <Moon className="mr-2 h-4 w-4" />
+          <span>Dark</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleThemeChange('system')}>
+          <Laptop className="mr-2 h-4 w-4" />
+          <span>System</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
